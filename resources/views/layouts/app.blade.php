3a85e9dcbf9d60bacefc0932680d5c3bb613f424
far-han12 <mahmudfarhan590@gmail.com>
2026-03-16 02:04:41 +0600

    Worked on admin User Management and in mock test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" x-data="{ dark: localStorage.theme === 'dark', open: false }" x-init="$watch('dark', v => { localStorage.theme = v ? 'dark' : 'light'; document.documentElement.classList.toggle('dark', v); }); document.documentElement.classList.toggle('dark', dark)">
 
 <head>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
